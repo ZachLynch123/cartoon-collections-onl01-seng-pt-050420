99a5dd15741209b063633b192d927cd2b01e11e4
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-11 18:14:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ def summon_captain_planet(collection)
   new_collection
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  
 end
 
 def find_the_cheese# code an argument here
