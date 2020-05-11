164b63aef97ad4cac0a94283d067382bb5782034
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-11 18:39:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ def find_the_cheese(snacc)
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0 
   while i < snacc.length
-    if snacc.include?(cheese_types[i])
+    if cheese_types.in?()
       x = 
       i +=1 
       return cheese_types[i] 
