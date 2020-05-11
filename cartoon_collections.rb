8d65160901e069a1f3fa0394b6ca4a5e4e560116
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-11 18:06:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,3 +22,5 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+summon_captain_planet(["Earth!", "Wind!", "Fire!", "Water!", "Heart!"])
